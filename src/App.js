5acf1ef2d59a079ebf8cc4ff4f8e95bd458a9c95
moophhd <d_sizykh@mail.ru>
2020-05-31 19:45:18 +0300

    inputs for row&col count, interval length, generation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React, { useState, useCallback, useRef } from "react";
+import React, { useState, useCallback, useRef, useEffect } from "react";
 import { produce } from 'immer';
 
-const numRows = 20;
-const numCols = 20;
 const operations = [
   [0, 1],
   [0, -1],
@@ -13,30 +11,46 @@ const operations = [
   [-1, 0],
   [-1, -1]
 ]
-const countNeighbors = (grid, x, y) => {
-  return operations.reduce((acc, [i, j]) => {
-    const row = (x + i + numRows) % numRows;
-    const col = (y + j + numCols) % numCols;
-    acc += grid[row][col];
-    return acc;
-  }, 0);
-};
 
 const App = () => {
-  const [grid, setGrid] = useState(Array.from({length: numRows}).map(() => Array.from({length: numCols}).fill(0)));
-  const [running, setRunning] = useState(false);
+  const generateEmptyGrid = (numRows, numCols) => {
+    return Array.from({length: numRows}).map(() => Array.from({length: numCols}).fill(0));
+  }
 
+  const [running, setRunning] = useState(false);
+  const [numRows, setNumRows] = useState(20);
+  const [numCols, setNumCols] = useState(20);
+  const [interval, setInterval] = useState(1000);
+  const [grid, setGrid] = useState(generateEmptyGrid(numRows, numCols));
+  
   const runningRef = useRef(running);
   runningRef.current = running; 
+  const intervalRef = useRef(interval);
+  intervalRef.current = interval;
+  const numRowsRef = useRef(numRows);
+  numRowsRef.current = numRows; 
+  const numColsRef = useRef(numCols);
+  numColsRef.current = numCols; 
+
+  const countNeighbors = (grid, x, y, numRows, numCols) => {
+    return operations.reduce((acc, [i, j]) => {
+      const row = (x + i + numRows) % numRows;
+      const col = (y + j + numCols) % numCols;
+      acc += grid[row][col];
+      return acc;
+    }, 0);
+  };
   
   const runSimulation = useCallback(() => {
     if (!runningRef.current) return;
 
+    const numRows = numRowsRef.current;
+    const numCols = numColsRef.current;
+
     setGrid(grid => produce(grid, newGrid => {
       for (let i = 0; i < numRows; i++) {
         for (let k = 0; k < numCols; k++) {
-          let neighbours = countNeighbors(grid, i, k);
-
+          let neighbours = countNeighbors(grid, i, k, numRows, numCols);
           if (neighbours < 2 || neighbours > 3) {
             newGrid[i][k] = 0;
           } else if (newGrid[i][k] === 0 && neighbours == 3) {
@@ -46,10 +60,13 @@ const App = () => {
       }
     }));
 
-    setTimeout(runSimulation, 500);
+    setTimeout(runSimulation, intervalRef.current);
   }, []);
 
-  
+  useEffect(() => {
+    setGrid(generateEmptyGrid(numRows, numCols));
+  }, [numCols, numRows])
+
   return (
     <div>
       <button onClick= {() => {
@@ -61,6 +78,51 @@ const App = () => {
       }}>
         {running ? "stop" : "start"}
       </button>
+
+      <button 
+        onClick={() => {
+          setGrid(generateEmptyGrid(numCols, numRows))
+       }}
+      >
+        clear
+      </button>
+
+      <button 
+        onClick={() => {
+          const grid = [];
+          for (let i = 0; i < numRows; i++) {
+            grid.push(Array.from(Array(numCols), () => Math.random() > .7 ? 1 : 0));
+          }
+
+          setGrid(grid);
+        }}
+      >
+        generate
+      </button>
+      rows:
+      <input
+        placeholder={numRows}
+        onChange={(e) => { 
+          setRunning(false);
+          setNumRows(parseInt(e.target.value))
+        }}
+      />
+      cols:
+      <input
+        placeholder={numCols}
+        onChange={(e) => { 
+          setRunning(false);
+          setNumCols(parseInt(e.target.value))
+        }}
+      />
+      speed:
+      <input 
+        style={{direction: "rtl"}}
+        type="range" min="50" max="1050" step={100} value={interval}
+        onInput={(e) => setInterval(e.target.value)}
+        onChange={(e) => setInterval(e.target.value)}
+      />
+
       <div style={{
         gridTemplateColumns: `repeat(${numCols}, 20px)`,
         display: 'grid',
